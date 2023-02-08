58dde6bab0a27becec91822e5dd264a344b2e085
WilberRojas <wilberlearn@gmail.com>
2023-02-08 19:27:35 -0400

    circle_v1

--- kcp/scripts/config_rviz/whiteboard_bridge.py
+++ kcp/scripts/config_rviz/whiteboard_bridge.py
@@ -2,7 +2,7 @@
 import rospy
 from sensor_msgs.msg import JointState
 
-rospy.init_node('joint_states2', anonymous=True)
+rospy.init_node('whiteboard_bridge', anonymous=True)
 pub_cleaner = rospy.Publisher('custom_joint_states', JointState, queue_size=1)
 
 def callback(data):
